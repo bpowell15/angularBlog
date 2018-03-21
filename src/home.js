61399f8c17f7e90470f099fc1e2c713be42ed55a
bpowell15 <bpaulpowell@gmail.com>
2018-03-21 13:32:12 -0400

    Add error messages

--- src/home.js
+++ src/home.js
@@ -12,15 +12,24 @@ module.exports = function($scope, $http, $localStorage) {
   $scope.submit = function (title, body, link) {
     $http.post('https://jsonplaceholder.typicode.com/posts').
           then(function(response) {
-            if (title.length === 0 || body.length === 0) {
+            if (title === undefined || body === undefined || title === "" || body === "") {
+              $scope.titleError = "Title can't be blank.";
+              if (body === undefined || body === "") {
+                $scope.bodyError = "Body can't be blank.";
+              }
               return;
             }
               $localStorage.posts = $scope.allPosts;
               $localStorage.posts.unshift({id: parseInt($localStorage.posts[$localStorage.posts.length - 1].id) + 1, title: title, body: body, userId: 3.14159});
               $localStorage.use = true;
+              $scope.titleError = "";
               link.$open = !link.$open;
           });
   };
 
-
+  $scope.close = function(link) {
+    link.$open = !link.$open;
+    $scope.titleError='';
+    $scope.bodyError='';
+  };
 };
